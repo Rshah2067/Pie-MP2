a88b4ebff3c469436e8ddf4238ab8412141c10f4
Rohan Shah <rohanshah2067@gmail.com>
2024-09-24 16:10:38 -0400

    tested reading a line of serial input

--- serialtest.py
+++ serialtest.py
@@ -1,3 +1,8 @@
 import serial
-ser = serial.Serial('/dev/ttyUSB0')
-#Read 10 bytes from buffer
+
+# Should I add a timeout
+ser = serial.Serial("/dev/ttyUSB0")
+while 1 == 1:
+    string = ser.readline()
+    print(string)
+#    Read 10 bytes from buffer
